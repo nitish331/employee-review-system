0f56965ee401696e700e6ebc489e5e7c069d1ae7
NITISH <108567260+nitish331@users.noreply.github.com>
2025-04-15 13:51:08 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 //connecting to the database
 mongoose.connect(
-  "mongodb+srv://Nitish:nitish33@cluster0.ksobj.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+  "mongodb+srv://Nitish:nitish33@cluster0.ksobj.mongodb.net/Ecommerce2?retryWrites=true&w=majority"
 );
 
 // acquiring the connection to check if it is succesfull
